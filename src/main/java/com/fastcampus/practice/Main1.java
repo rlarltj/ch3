7dda50ad1f5d004215289b1445dc417403dd1267
kiseo <5rne@naver.com>
2022-02-19 23:59:52 +0900

    practice

---
+++ src/main/java/com/fastcampus/practice/Main1.java
@@ -0,0 +1,26 @@
+package com.fastcampus.practice;
+
+import java.io.FileReader;
+import java.util.Properties;
+
+class Car{}
+class SportCar extends Car{}
+class Truck extends Car{}
+class Engine{}
+public class Main1 {
+    public static void main(String[] args) throws Exception{
+        Car car = (Car)getObject("car");
+        Engine engine = (Engine) getObject("engine");
+        System.out.println("car = " + car);
+        System.out.println("engine = " + engine);
+    }
+
+    static Object getObject(String key) throws Exception{
+        Properties p = new Properties();
+        p.load(new FileReader("config.txt"));
+
+        Class clazz = Class.forName(p.getProperty(key));
+
+        return clazz.newInstance();
+    }
+}
